883ddc276dbf5de7772c03bb362bff9f0e22f4ea
Thomas Off <t.off@datamints.com>
2020-09-17 12:37:17 +0200

    Wrap initialization code with function (modern style).

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,131 +2,133 @@
 
 defined('TYPO3_MODE') or die();
 
-$_LLL_be = 'LLL:EXT:cart_products/Resources/Private/Language/locallang_be.xlf:';
+call_user_func(function ($extensionKey) {
+    $_LLL_be = 'LLL:EXT:cart_products/Resources/Private/Language/locallang_be.xlf:';
 
-// configure plugins
+    // configure plugins
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'Extcode.cart_products',
-    'Products',
-    [
-        'Product' => 'show, list, teaser, showForm',
-    ],
-    [
-        'Product' => 'showForm',
-    ]
-);
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'Extcode.cart_products',
-    'TeaserProducts',
-    [
-        'Product' => 'teaser, showForm',
-    ],
-    [
-        'Product' => 'showForm',
-    ]
-);
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'Extcode.cart_products',
-    'SingleProduct',
-    [
-        'Product' => 'show, showForm',
-    ],
-    [
-        'Product' => 'showForm',
-    ]
-);
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'Extcode.cart_products',
-    'ProductPartial',
-    [
-        'Product' => 'showForm',
-    ],
-    [
-        'Product' => 'showForm',
-    ]
-);
-
-// Icon Registry
-
-if (TYPO3_MODE === 'BE') {
-    $icons = [
-        'apps-pagetree-folder-cartproducts-products' => 'apps_pagetree_folder_cartproducts_products.svg',
-        'apps-pagetree-page-cartproducts-products' => 'apps_pagetree_page_cartproducts_products.svg',
-        'ext-cartproducts-wizard-icon' => 'cartproducts_plugin_wizard.svg',
-    ];
-
-    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-        \TYPO3\CMS\Core\Imaging\IconRegistry::class
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'Extcode.cart_products',
+        'Products',
+        [
+            'Product' => 'show, list, teaser, showForm',
+        ],
+        [
+            'Product' => 'showForm',
+        ]
     );
 
-    foreach ($icons as $identifier => $fileName) {
-        $iconRegistry->registerIcon(
-            $identifier,
-            \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-            [
-                'source' => 'EXT:cart_products/Resources/Public/Icons/' . $fileName,
-            ]
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'Extcode.cart_products',
+        'TeaserProducts',
+        [
+            'Product' => 'teaser, showForm',
+        ],
+        [
+            'Product' => 'showForm',
+        ]
+    );
+
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'Extcode.cart_products',
+        'SingleProduct',
+        [
+            'Product' => 'show, showForm',
+        ],
+        [
+            'Product' => 'showForm',
+        ]
+    );
+
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'Extcode.cart_products',
+        'ProductPartial',
+        [
+            'Product' => 'showForm',
+        ],
+        [
+            'Product' => 'showForm',
+        ]
+    );
+
+    // Icon Registry
+
+    if (TYPO3_MODE === 'BE') {
+        $icons = [
+            'apps-pagetree-folder-cartproducts-products' => 'apps_pagetree_folder_cartproducts_products.svg',
+            'apps-pagetree-page-cartproducts-products' => 'apps_pagetree_page_cartproducts_products.svg',
+            'ext-cartproducts-wizard-icon' => 'cartproducts_plugin_wizard.svg',
+        ];
+
+        $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+            \TYPO3\CMS\Core\Imaging\IconRegistry::class
         );
+
+        foreach ($icons as $identifier => $fileName) {
+            $iconRegistry->registerIcon(
+                $identifier,
+                \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+                [
+                    'source' => 'EXT:cart_products/Resources/Public/Icons/' . $fileName,
+                ]
+            );
+        }
     }
-}
 
-// TSconfig
+    // TSconfig
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
-    <INCLUDE_TYPOSCRIPT: source="FILE:EXT:cart_products/Configuration/TSconfig/ContentElementWizard.tsconfig">
-');
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
+        <INCLUDE_TYPOSCRIPT: source="FILE:EXT:cart_products/Configuration/TSconfig/ContentElementWizard.tsconfig">
+    ');
 
-// Cart Hooks
+    // Cart Hooks
 
-if (TYPO3_MODE === 'FE') {
-    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['cart']['CartProducts']['Cart']['AddToCartFinisher'] =
-        \Extcode\CartProducts\Domain\Finisher\Cart\AddToCartFinisher::class;
-}
+    if (TYPO3_MODE === 'FE') {
+        $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['cart']['CartProducts']['Cart']['AddToCartFinisher'] =
+            \Extcode\CartProducts\Domain\Finisher\Cart\AddToCartFinisher::class;
+    }
 
-// ke_search Hook - register indexer for products
+    // ke_search Hook - register indexer for products
 
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['registerIndexerConfiguration'][] =
-    \Extcode\CartProducts\Hooks\KeSearchIndexer::class;
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['customIndexer'][] =
-    \Extcode\CartProducts\Hooks\KeSearchIndexer::class;
+    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['registerIndexerConfiguration'][] =
+        \Extcode\CartProducts\Hooks\KeSearchIndexer::class;
+    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['customIndexer'][] =
+        \Extcode\CartProducts\Hooks\KeSearchIndexer::class;
 
-// processDatamapClass Hook
+    // processDatamapClass Hook
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['cartproducts_allowed'] =
-    \Extcode\CartProducts\Hooks\DatamapDataHandlerHook::class;
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['cartproducts_allowed'] =
+        \Extcode\CartProducts\Hooks\DatamapDataHandlerHook::class;
 
-// clearCachePostProc Hook
+    // clearCachePostProc Hook
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc']['cartproducts_clearcache'] =
-    \Extcode\CartProducts\Hooks\DataHandler::class . '->clearCachePostProc';
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc']['cartproducts_clearcache'] =
+        \Extcode\CartProducts\Hooks\DataHandler::class . '->clearCachePostProc';
 
-// Signal Slots
+    // Signal Slots
 
-$dispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-    \TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class
-);
+    $dispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+        \TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class
+    );
 
-$dispatcher->connect(
-    \Extcode\Cart\Utility\StockUtility::class,
-    'handleStock',
-    \Extcode\CartProducts\Utility\StockUtility::class,
-    'handleStock'
-);
+    $dispatcher->connect(
+        \Extcode\Cart\Utility\StockUtility::class,
+        'handleStock',
+        \Extcode\CartProducts\Utility\StockUtility::class,
+        'handleStock'
+    );
 
-// register "cartproducts:" namespace
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['cartproducts'][]
-    = 'Extcode\\CartProducts\\ViewHelpers';
+    // register "cartproducts:" namespace
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['cartproducts'][]
+        = 'Extcode\\CartProducts\\ViewHelpers';
 
-// update wizard for slugs
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['cartProductsSlugUpdater'] =
-    \Extcode\CartProducts\Updates\SlugUpdater::class;
+    // update wizard for slugs
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['cartProductsSlugUpdater'] =
+        \Extcode\CartProducts\Updates\SlugUpdater::class;
 
-// register listTemplateLayouts
-$GLOBALS['TYPO3_CONF_VARS']['EXT']['cart_products']['templateLayouts']['products'][] = [$_LLL_be . 'flexforms_template.templateLayout.products.table', 'table'];
-$GLOBALS['TYPO3_CONF_VARS']['EXT']['cart_products']['templateLayouts']['products'][] = [$_LLL_be . 'flexforms_template.templateLayout.products.grid', 'grid'];
-$GLOBALS['TYPO3_CONF_VARS']['EXT']['cart_products']['templateLayouts']['teaser_products'][] = [$_LLL_be . 'flexforms_template.templateLayout.products.table', 'table'];
-$GLOBALS['TYPO3_CONF_VARS']['EXT']['cart_products']['templateLayouts']['teaser_products'][] = [$_LLL_be . 'flexforms_template.templateLayout.products.grid', 'grid'];
+    // register listTemplateLayouts
+    $GLOBALS['TYPO3_CONF_VARS']['EXT']['cart_products']['templateLayouts']['products'][] = [$_LLL_be . 'flexforms_template.templateLayout.products.table', 'table'];
+    $GLOBALS['TYPO3_CONF_VARS']['EXT']['cart_products']['templateLayouts']['products'][] = [$_LLL_be . 'flexforms_template.templateLayout.products.grid', 'grid'];
+    $GLOBALS['TYPO3_CONF_VARS']['EXT']['cart_products']['templateLayouts']['teaser_products'][] = [$_LLL_be . 'flexforms_template.templateLayout.products.table', 'table'];
+    $GLOBALS['TYPO3_CONF_VARS']['EXT']['cart_products']['templateLayouts']['teaser_products'][] = [$_LLL_be . 'flexforms_template.templateLayout.products.grid', 'grid'];
+}, 'cart_products');
--- ext_tables.php
+++ ext_tables.php
@@ -2,41 +2,43 @@
 
 defined('TYPO3_MODE') or die();
 
-$iconPath = 'EXT:cart_products/Resources/Public/Icons/';
-$_LLL_be = 'LLL:EXT:cart_products/Resources/Private/Language/locallang_be.xlf:';
+call_user_func(function ($extensionKey) {
+    $iconPath = 'EXT:cart_products/Resources/Public/Icons/';
+    $_LLL_be = 'LLL:EXT:cart_products/Resources/Private/Language/locallang_be.xlf:';
 
-/**
- * Register Backend Modules
- */
-if (TYPO3_MODE === 'BE') {
-    if (!isset($TBE_MODULES['Cart'])) {
-        $temp_TBE_MODULES = [];
-        foreach ($TBE_MODULES as $key => $val) {
-            if ($key == 'file') {
-                $temp_TBE_MODULES[$key] = $val;
-                $temp_TBE_MODULES['Cart'] = '';
-            } else {
-                $temp_TBE_MODULES[$key] = $val;
+    /**
+     * Register Backend Modules
+     */
+    if (TYPO3_MODE === 'BE') {
+        if (!isset($TBE_MODULES['Cart'])) {
+            $temp_TBE_MODULES = [];
+            foreach ($TBE_MODULES as $key => $val) {
+                if ($key == 'file') {
+                    $temp_TBE_MODULES[$key] = $val;
+                    $temp_TBE_MODULES['Cart'] = '';
+                } else {
+                    $temp_TBE_MODULES[$key] = $val;
+                }
             }
+
+            $TBE_MODULES = $temp_TBE_MODULES;
         }
 
-        $TBE_MODULES = $temp_TBE_MODULES;
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+            'Extcode.cart_products',
+            'Cart',
+            'Products',
+            '',
+            [
+                'Backend\Product' => 'list, show,',
+                'Backend\Variant' => 'list, show, edit, update',
+            ],
+            [
+                'access' => 'user, group',
+                'icon' => $iconPath . 'module_products.svg',
+                'labels' => $_LLL_be . 'tx_cartproducts.module.products',
+                'navigationComponentId' => 'TYPO3/CMS/Backend/PageTree/PageTreeElement',
+            ]
+        );
     }
-
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-        'Extcode.cart_products',
-        'Cart',
-        'Products',
-        '',
-        [
-            'Backend\Product' => 'list, show,',
-            'Backend\Variant' => 'list, show, edit, update',
-        ],
-        [
-            'access' => 'user, group',
-            'icon' => $iconPath . 'module_products.svg',
-            'labels' => $_LLL_be . 'tx_cartproducts.module.products',
-            'navigationComponentId' => 'TYPO3/CMS/Backend/PageTree/PageTreeElement',
-        ]
-    );
-}
+}, 'cart_products');
